3f1280faac24f3a8756a0f70dc94a4d993f74239
Anthony Tsang <anthony.tsang@soldocoin.com>
2016-03-29 03:36:27 +0800

    fix deploy script

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@ git push origin HEAD
 git checkout gh-pages
 git merge master --no-edit
 gulp build
-git add .
+git add dist
 git commit -a -m "sync master"
 git subtree push --prefix dist origin gh-pages
 git checkout master
